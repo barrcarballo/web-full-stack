1ebf1876dc1659f9d0ed305a343715a4059b1b2f
aguandrada12 <agu.andradaqwer@gmail.com>
2025-10-05 19:03:10 -0300

    Update header.jsx

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,7 +1,8 @@
 import '../styles/stylesheet.css';
+/* import { Link } from "react-router-dom"; */
 //Falta importar el script con las rutas de las imagenes y enlaces.
 
-function Header() {
+function Header({cantidadCarrito}) {
   return(
     <header>
       <nav>
@@ -14,9 +15,19 @@ function Header() {
           <a href="">Nosotros</a>
           <a href="">Contacto</a>
           <a href="" className="cart">
+            
+          <Link to="/">Home</Link>
+          <Link to="/productos">Productos</Link>
+          <Link to="/nosotros">Nosotros</Link>
+          <Link to="/contacto">Contacto</Link>
+          <Link to="/carrito" className="cart">  
+          (ESTO DE ARRIBA CREO QUE HABRIA QUE CAMBIARLO POR LO DE LOS LINKS QUE TENIAMOS ANTES POR LO DE ROUTES)
+          
+          
             <img src="images/Carrito.svg" alt="carrito" />
-            <span id="contador">0</span>
+            <span id="contador">{cantidadCarrito}</span>
           </a>
+          </Link>
         </div>
       </nav>
   </header>
