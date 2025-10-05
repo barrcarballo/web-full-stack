1be7dfb0d6ac9abcda3708344d4a97f50d2912d7
aguandrada12 <agu.andradaqwer@gmail.com>
2025-10-05 14:28:00 -0300

    Update contacto.jsx

--- client/src/pages/contacto.jsx
+++ client/src/pages/contacto.jsx
@@ -1,1 +1,15 @@
+import Header from '../components/header.jsx';
+import Footer from '../components/footer.jsx';
+import ContactForm from '../components/ContactForm.jsx';
 
+function Contacto() {
+  return (
+    <>
+      <Header />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+}
+
+export default Contacto;
